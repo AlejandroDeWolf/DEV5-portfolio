62eaee24c5966f464ae1e657611209d896290d2e
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-10-12 18:54:30 +0200

    feat: replace cube with plane

--- lab4-threejs/main.js
+++ lab4-threejs/main.js
@@ -36,18 +36,20 @@ scene.add(directionalLight);
 const lightHelper = new THREE.DirectionalLightHelper(directionalLight);
 scene.add(lightHelper);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.PlaneGeometry(1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
 
-camera.position.z = 5;
+camera.position.z = 8;
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  controls.update();
 
   renderer.render(scene, camera);
 }
