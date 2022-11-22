8ab1b40e87fae91c6f29e26010701ca5a8a60e16
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-11-22 14:20:28 +0100

    feat: input field to add comment to comments array and post to api

--- lab6-vue/src/components/Comments.vue
+++ lab6-vue/src/components/Comments.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive, ref } from "vue";
 
 let api_url = "https://lab5-p379.onrender.com/api/v1/messages/";
 let comments = reactive({ comments: [] });
+let comment = ref("");
 
 onMounted(() => {
   fetch(api_url)
@@ -11,6 +12,22 @@ onMounted(() => {
       comments.comments = data;
     });
 });
+
+// push comment and username to comments array and post to api
+const addComment = () => {
+  comments.comments.push({ user: "Alejandro", text: comment.value });
+  fetch(api_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: "Alejandro",
+      text: comment.value,
+    }),
+  });
+  comment.value = "";
+};
 </script>
 
 <template>
@@ -19,6 +36,10 @@ onMounted(() => {
     <div v-for="comment in comments.comments" :key="comment.id">
       <h3>{{ comment.user }}</h3>
       <p>{{ comment.text }}</p>
+    </div>
+    <div>
+      <input type="text" v-model="comment" />
+      <button @click="addComment">Add Comment</button>
     </div>
   </div>
 </template>
